cedf6b775117065f37325f6bb7762dd6dfdf548a
Taek Seung Lee <dlxortmd987@gmail.com>
2024-06-01 17:33:20 +0900

    [project] mybatis batch

---
+++ projects/spring/mybatis-batch/src/test/java/com/example/mybatisbatch/MybatisBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatisbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
