5e3908279f75579f7e2abb926ca824987a370c58
Taek Seung Lee <dlxortmd987@gmail.com>
2024-06-01 22:27:06 +0900

    [project] mybatis batch Executor.BATCH 방식의 배치 insert 케이스 작성

--- projects/spring/mybatis-batch/src/test/java/com/example/mybatisbatch/mapper/BatchInsertTest.java
+++ projects/spring/mybatis-batch/src/test/java/com/example/mybatisbatch/mapper/BatchInsertTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,14 +21,17 @@ import com.example.mybatisbatch.domain.Computer;
 @Transactional
 class BatchInsertTest {
 
+	private static final int BATCH_SIZE = 100_000;
 	@Autowired
 	private ComputerMapper computerMapper;
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Test
 	void for_문으로_삽입하는_경우() {
-		List<Computer> computers = COMPUTERS_FIXTURE(1000);
+		List<Computer> computers = COMPUTERS_FIXTURE(BATCH_SIZE);
 
 		long start = System.currentTimeMillis();
 		for (Computer computer : computers) {
@@ -40,8 +46,8 @@ class BatchInsertTest {
 	}
 
 	@Test
-	void mybatic_foreach로_삽입하는_경우() {
-		List<Computer> computers = COMPUTERS_FIXTURE(1000);
+	void mybatis_foreach로_삽입하는_경우() {
+		List<Computer> computers = COMPUTERS_FIXTURE(BATCH_SIZE);
 
 		long start = System.currentTimeMillis();
 		computerMapper.insertBatch(computers);
@@ -51,6 +57,25 @@ class BatchInsertTest {
 		List<Computer> actual = computerMapper.selectAll();
 		assertThat(actual).usingRecursiveComparison().isEqualTo(computers);
 		log.info("----------걸린 시간: {} (ms)", (end - start));
+	}
+
+	@Test
+	void ExcuteType을_Batch_로_삽입하는_경우() {
+		List<Computer> computers = COMPUTERS_FIXTURE(BATCH_SIZE);
+
+		try (SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH)) {
+			ComputerMapper computerMapper = sqlSession.getMapper(ComputerMapper.class);
+			long start = System.currentTimeMillis();
+			for (int i = 0; i < computers.size(); i++) {
+				computerMapper.insert(computers.get(i));
+			}
+			sqlSession.commit();
+			long end = System.currentTimeMillis();
+
+			List<Computer> actual = computerMapper.selectAll();
+			assertThat(actual).usingRecursiveComparison().isEqualTo(computers);
+			log.info("----------걸린 시간: {} (ms)", (end - start));
+		}
 	}
 
 	private List<Computer> COMPUTERS_FIXTURE(int size) {
